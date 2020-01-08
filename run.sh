138d8d92f0a9ebbf1401a9ed61c728a903cd4541
steven <onek@me.com>
2020-01-08 09:34:30 +0900

    add Jenkinsfile

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #
-#    Copyright 2010-2019 the original author or authors.
+#    Copyright 2010-2020 the original author or authors.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -20,3 +20,6 @@ docker push onek0708/jpetstore:v3
 kubectl apply -f deployment.yaml -n jpetstore
 #kubectl apply -f service.yaml -n jpetstore
 #kubectl apply -f ingress.yaml -n jpetstore
+
+kubectl create configmap tomcat-manager --from-file=manager.xml -n jpetstore
+kubectl create configmap tomcat-users --from-file=tomcat-users.xml -n jpetstore
